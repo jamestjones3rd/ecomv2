47296ecf213d70820a493b37c7f253a8e5cb7858
James Jones <jamesjones@Administrators-MacBook-Pro.local>
2023-07-18 21:20:17 -0400

    yep

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,22 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+// this is for deployment
+if (process.env.NODE_ENV === 'production') {
+	const __dirname = path.resolve();
+	app.use('/uploads', express.static('/var/data/uploads'));
+	app.use(express.static(path.join(__dirname, '/frontend/build')));
+  
+	app.get('*', (req, res) =>
+	  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+	);
+  } else {
+	const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+	app.get('/', (req, res) => {
+	  res.send('API is running....');
+	});
+  }
 
 app.get("/", (req, res) => {
 	res.send(`API is running...`);
